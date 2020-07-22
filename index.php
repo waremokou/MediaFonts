2d785649ced257f9e10c4459db5b3741f591ea4a
waremokou <waremokou.kurenai@gmail.com>
2020-07-22 13:10:58 +0900

    Revert "Revert "ファイルをアップロード""
    This reverts commit adf7439620090666978d6596a24f60825f571344.

---
+++ index.php
@@ -0,0 +1,170 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>わたしのすきなフリーフォントたち</title>
+
+    <meta name="description" content="わたしが好きなフリーフォントをたくさん紹介しています。明朝体、ゴシック体から手書き風まで、どれも素敵なフォントたちです。" />
+    <meta name="keywords" content="フォント,フリーフォント,デザイン" />
+
+  	<meta property="og:title" content="わたしのすきなフリーフォントたち" />
+  	<meta property="og:type" content="website" />
+  	<meta property="og:url" content=" ページの URL" />
+  	<meta property="og:image" content="./common/img/seo/img.jpg" />
+  	<meta property="og:site_name" content="わたしのすきなフリーフォントたち" />
+  	<meta property="og:description" content="わたしが好きなフリーフォントをたくさん紹介しています。明朝体、ゴシック体から手書き風まで、どれも素敵なフォントたちです。" />
+
+  	<!--  Facebook用設定 -->
+  	<meta property="fb:admins" content="adminID（15文字の半角数字）" />
+
+  	<!-- ※ Twitter共通設定 -->
+  	<meta name="twitter:card" content="summary_large_image" />
+
+    <script src="common/js/import.js"></script>
+    <link href="common/css/import.css" rel="stylesheet">
+    <link rel="icon" href="./favicon.ico">
+
+  </head>
+  <body>
+    <div class="stripeLine headerFix">
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+
+	  <header>
+      <div>
+        <h1>
+          <a href="./index.html"><img src="./common/img/main/logo.png" alt="「わたしのすきなフリーフォントたち」のロゴ"></a>
+        </h1>
+
+        <nav>
+          <a href="" target="_blank"><i class="fab fa-twitter-square"></i></a>
+          <a href="" target="_blank"><i class="fab fa-facebook-square"></i></a>
+          <p>
+            <a href="./contact.html">お問い合わせ</a>
+          </p>
+          <div class="hamburger">
+            <span></span>
+          </div>
+          <ul>
+            <li><a href="./archive.html">ゴシック体</a></li>
+            <li><a href="">明朝体</a></li>
+            <li><a href="">手書き風</a></li>
+            <li><a href="">商用利用可</a></li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+
+    <div class="indexHeader headerMargin_index">
+      <div class="indexHeader_square"></div>
+      <p>
+        わたしのすきな<br>
+        フリーフォントたち
+      </p>
+    </div>
+
+    <div class="mainWrap">
+      <p>
+        デザイナーにとって欠かせないフォント。<br>
+        そんなフォントの中でも無料で使えるフリーフォントをピックアップしてまとめました。
+      </p>
+
+      <article>
+
+        <ul class="mainFlex">
+          <li>
+            <a href="./single.html">
+              <dl>
+                <dt><img src="./common/img/single/f_timemachine-wa_main.jpg" alt=""></dt>
+                <dd>タイムマシンわ号</dd>
+                <dd>ゴシック体</dd>
+                <dd>商用：○</dd>
+              </dl>
+            </a>
+          </li>
+
+          <li>
+            <a href="">
+              <dl>
+                <dt><img src="./common/img/single/kv_memo_s.png" alt=""></dt>
+                <dd>仕事メモ書き</dd>
+                <dd>手書き</dd>
+                <dd>商用：○</dd>
+              </dl>
+            </a>
+          </li>
+
+          <li>
+            <a href="">
+              <dl>
+                <dt><img src="./common/img/single/soukou-Mincho-font.png" alt=""></dt>
+                <dd>装甲明朝</dd>
+                <dd>明朝体</dd>
+                <dd>商用：○</dd>
+              </dl>
+            </a>
+          </li>
+
+          <li>
+            <a href="">
+              <dl>
+                <dt><img src="./common/img/single/nihongo_image2.gif" alt=""></dt>
+                <dd>フォントポにほんご</dd>
+                <dd>ゴシック体</dd>
+                <dd>商用：○</dd>
+              </dl>
+            </a>
+          </li>
+
+          <li>
+            <a href="">
+              <dl>
+                <dt><img src="./common/img/single/ex-theatrism.jpg" alt=""></dt>
+                <dd>てあとりずむ</dd>
+                <dd>ゴシック体</dd>
+                <dd>商用：×</dd>
+              </dl>
+            </a>
+          </li>
+
+          <li>
+            <a href="">
+              <dl>
+                <dt><img src="./common/img/single/f_cinderella_main.jpg" alt=""></dt>
+                <dd>シンデレラ</dd>
+                <dd>ゴシック体</dd>
+                <dd>商用：○</dd>
+              </dl>
+            </a>
+          </li>
+        </ul>
+
+      </article>
+    </div>
+
+  <footer>
+    <small>
+      powered by WordPress
+    </small>
+
+    <div class="stripeLine">
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  </footer>
+
+  </body>
+</html>
